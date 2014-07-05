9a79c143d300bd60226c90d02be0929d0bbb5e7d
Alex <alexpastiu@gmail.com>
2014-07-05 16:01:16 -0400

    all game rules working, and all debug alerts commented out. PLAY GO

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ app.controller('GoBoardController', function($scope){
 
 //GLOBAL VARIABLES
 	$scope.globalVar = {
-		boardLength: 9,			//GLOBAL set boardLength
+		boardLength: 6,			//GLOBAL set boardLength
 		turnNumber: 1,			//turn counter(used to switch player)
 		turnColor: 'black',
 		oppositeTurnColor: 'white',
@@ -13,12 +13,23 @@ app.controller('GoBoardController', function($scope){
 		anyEmptyFlag: false,
 		enemySurroundFlag: false,
 		tookEnemyFlag: false,
-		anyEmptyCounter: 0
+		anyEmptyCounter: 0,
+		blackScore: 0,
+		whiteScore: 0,
+		passCounter: 0,
+		piecesTakenThisTurn: 0,
+		idsTakenThisTurn: []
 	}
 
+//PASS EVENT
 	$scope.pass = function () {
-		$scope.globalVar.turnColor = $scope.globalVar.turnColor == 'white' ? 'black' : 'white';
-		$scope.globalVar.turnNumber++;}
+		$scope.globalVar.turnColor = oppositeTurnColor($scope.globalVar.turnColor);
+		$scope.globalVar.turnNumber++;
+		$scope.globalVar.passCounter++;
+		if($scope.globalVar.passCounter == 2){
+			alert('BOTH PLAYERS PASS' + '\n' + 'count your territory and add it to your score');
+		}
+	}
 
 //MOUSE UP EVENT
 	$scope.mouseUpFunction = function(cell, row, col){
@@ -28,34 +39,58 @@ app.controller('GoBoardController', function($scope){
 			$scope.globalVar.enemySurroundFlag = false;		//reset the enemySurroundFlag
 			$scope.globalVar.tookEnemyFlag = false;			//reset the tookEnemyFlag
 			$scope.globalVar.anyEmptyCounter = 0;			//reset the anyEmptyCounter
+			$scope.globalVar.piecesTakenThisTurn = 0;		//reset the piecesTakenThisTurn
 
-			resetCheckedStatus($scope.goBoard);
-			resetToTakeStatus($scope.goBoard);
+			if($scope.globalVar.idsTakenThisTurn[0] == cell.idNum){
+				alert('illegal move, play again');
+				cell.colorStatus = 'emptySpot';
+				$scope.globalVar.turnNumber--;
+			}else{
+				$scope.globalVar.idsTakenThisTurn = [];			//reset the idsTakenThisTurn
+				resetCheckedStatus($scope.goBoard);
+				resetToTakeStatus($scope.goBoard);
 
-			$scope.globalVar.turnColor = getTurnColor($scope.globalVar.turnNumber, cell, $scope.globalVar.turnColor);
-			
-			checkNeighbours(cell, row, col, $scope.globalVar.boardLength, $scope.goBoard);
+				$scope.globalVar.turnColor = getTurnColor($scope.globalVar.turnNumber, cell, $scope.globalVar.turnColor);
+				
+				checkNeighbours(cell, row, col, $scope.globalVar.boardLength, $scope.goBoard);
 
-			checkSingleIllegal(cell);
-			checkMultipleIllegal(cell);
+				checkSingleIllegal(cell);
+				checkMultipleIllegal(cell);
+				illegalRepeatMove(cell);
+			}
 
 			$scope.globalVar.turnNumber++;
+			$scope.globalVar.passCounter = 0;
+			$scope.globalVar.piecesTakenThisTurn = 0;
 		}
 	}
 
-		//IF THE anyEmptyFlag is false.  YOU MUST CHECK SURROUNDING FRIENDLY PIECES USING RECURSION
-		//IF YOU FIND NO EMPTY SPOTS, IT'S AN ILLEGAL MOVE
+//CHECK FOR REPEAT MOVES
+	function illegalRepeatMove(cell){
+		if($scope.globalVar.piecesTakenThisTurn == 1){
+			//alert('you cant play on id#' + $scope.globalVar.idsTakenThisTurn[0]);
+		}else{
+			$scope.globalVar.idsTakenThisTurn = [];
+		}
+	}
 
 //CHECK anyEmptyCounter TO SEE IF YOU SHOULD TAKE ANY PIECES
 	function checkAnyEmptyCounter(anyEmptyCounter, goBoard){
-		alert('anyEmptyCounter = ' + anyEmptyCounter + '================================');
+		//alert('anyEmptyCounter = ' + anyEmptyCounter);
 		if(anyEmptyCounter == 0){
-			alert('YOU MUST DELETE SOME PIECES!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!');
+			//alert('Youve taken some pieces');
 			$scope.globalVar.tookEnemyFlag = true;
 			for(var i = 0; i < goBoard.length; i++){
 				for(var j = 0; j < goBoard[i].length; j++){
 					if(goBoard[i][j].toTakeStatus == true){
 						goBoard[i][j].toTakeStatus = false;
+						if(goBoard[i][j].colorStatus == 'black'){
+							$scope.globalVar.whiteScore++;
+						}else{
+							$scope.globalVar.blackScore++;
+						}
+						$scope.globalVar.piecesTakenThisTurn++;
+						($scope.globalVar.idsTakenThisTurn).push(goBoard[i][j].idNum);
 						goBoard[i][j].colorStatus = 'emptySpot';
 					}
 				}
@@ -82,36 +117,54 @@ app.controller('GoBoardController', function($scope){
 		}
 	}
 
+//CHECK FRIENDLIES FOR POTENTIAL ILLEGAL MOVE
+	function checkIllegalFriendly(cell, boardLength, goBoard){
+		resetCheckedStatus($scope.goBoard);
+		resetToTakeStatus($scope.goBoard);
+		$scope.globalVar.anyEmptyCounter = 0;
+
+		checkEnemies(cell, boardLength, goBoard);
+
+		if($scope.globalVar.anyEmptyCounter == 0){
+			alert('illegal move, play again');
+			cell.colorStatus = 'emptySpot';
+			$scope.globalVar.turnNumber--;
+			$scope.globalVar.turnColor = oppositeTurnColor($scope.globalVar.turnColor);
+		}
+
+		$scope.globalVar.anyEmptyCounter = 0;
+	}
+
 //CHECK ENEMIES  -----  the 'cell' is an object like 'goBoard[row][col]'
 	function checkEnemies(cell, boardLength, goBoard){
-		alert('checkEnemies().cell = ' + cell + '\n' +
-			'checkEnemies().cell.colorStatus = ' + cell.colorStatus + '\n' +
-			'checkEnemies().cell.checkedStatus = ' + cell.checkedStatus + '\n' +
-			'checkEnemies().cell.idNum = ' + cell.idNum);
+		//alert('checkEnemies().cell = ' + cell + '\n' +
+		//	'checkEnemies().cell.colorStatus = ' + cell.colorStatus + '\n' +
+		//	'checkEnemies().cell.checkedStatus = ' + cell.checkedStatus + '\n' +
+		//	'checkEnemies().cell.idNum = ' + cell.idNum);
 		if(cell.checkedStatus == false){
 			cell.checkedStatus = true;
 
 			if(cell.cellRow > 1){
-				alert('checkEnemies checkUp' + '\n' +
-					cell.cellRow + '-' + cell.cellCol + ' -> ' + (cell.cellRow-1) + '-' + cell.cellCol);
+				//alert('checkEnemies checkUp' + '\n' +
+				//	cell.cellRow + '-' + cell.cellCol + ' -> ' + (cell.cellRow-1) + '-' + cell.cellCol);
 				var focusPiece = goBoard[(cell.cellRow-2)][cell.cellCol-1];
 				ifCheckRecursion(focusPiece, cell);
 			}
 			if(cell.cellCol > 1){
-				alert('checkEnemies checkLeft' + '\n' +
-					cell.cellRow + '-' + cell.cellCol + ' -> ' + cell.cellRow + '-' + (cell.cellCol-1));
+				//alert('checkEnemies checkLeft' + '\n' +
+				//	cell.cellRow + '-' + cell.cellCol + ' -> ' + cell.cellRow + '-' + (cell.cellCol-1));
 				var focusPiece = goBoard[(cell.cellRow-1)][cell.cellCol-2];
 				ifCheckRecursion(focusPiece, cell);
 			}
 			if(cell.cellCol < boardLength){
-				alert('checkEnemies checkRight' + '\n' +
-					cell.cellRow + '-' + cell.cellCol + ' -> ' + cell.cellRow + '-' + (cell.cellCol+1));
+				//alert('checkEnemies checkRight' + '\n' +
+				//	cell.cellRow + '-' + cell.cellCol + ' -> ' + cell.cellRow + '-' + (cell.cellCol+1));
 				var focusPiece = goBoard[(cell.cellRow-1)][cell.cellCol];
 				ifCheckRecursion(focusPiece, cell);
 			}
 			if(cell.cellRow < boardLength){
-				alert('checkEnemies checkDown' + '\n' +
-					cell.cellRow + '-' + cell.cellCol + ' -> ' + (cell.cellRow+1) + '-' + cell.cellCol);
+				//alert('checkEnemies checkDown' + '\n' +
+				//	cell.cellRow + '-' + cell.cellCol + ' -> ' + (cell.cellRow+1) + '-' + cell.cellCol);
 				var focusPiece = goBoard[(cell.cellRow)][cell.cellCol-1];
 				ifCheckRecursion(focusPiece, cell);
 			}
@@ -122,20 +175,20 @@ app.controller('GoBoardController', function($scope){
 	function ifCheckRecursion(focusPiece, cell){
 		if(focusPiece.checkedStatus == false){
 			if(cell.colorStatus == focusPiece.colorStatus){
-				alert('triggering checkEnemies() for id#' + focusPiece.idNum);
+				//alert('triggering checkEnemies() for id#' + focusPiece.idNum);
 				focusPiece.toTakeStatus = true;
 				checkEnemies(focusPiece, $scope.globalVar.boardLength, $scope.goBoard);
 			}else if(focusPiece.colorStatus != 'emptySpot' && focusPiece.colorStatus != cell.colorStatus){
-				alert('found opposite here: ' + focusPiece.idNum);
+				//alert('found opposite here: ' + focusPiece.idNum);
 			}else if(focusPiece.colorStatus == 'emptySpot'){
-				alert('found empty.  moving on');
+				//alert('found empty.  moving on');
 				focusPiece.checkedStatus = true;
 				$scope.globalVar.anyEmptyCounter++;
 			}else{
 				alert('this should never have popped up');
 			}
 		}else{
-			alert('already checked this piece');
+			//alert('already checked this piece');
 		}
 	}
 
@@ -146,21 +199,21 @@ app.controller('GoBoardController', function($scope){
 		var immediateFriendlyCount = 0;
 		var spotsChecked = 0;
 
-		alert('looking at'+'\n'+'row: '+(row+1)+'\n'+'col: '+(col+1));
+		//alert('looking at'+'\n'+'row: '+(row+1)+'\n'+'col: '+(col+1));
 
 		if(row > 0){						//check up
 			var checkUp = goBoard[row-1][col];
 			if(checkUp.colorStatus == 'emptySpot'){
 				immediateEmptyCount++;
-				alert('checkUp found emptySpot');
+				//alert('checkUp found emptySpot');
 			}
 			if(checkUp.colorStatus == cell.colorStatus){
 				immediateFriendlyCount++;
-				alert('checkUp found friendly');
+				//alert('checkUp found friendly');
 			}
 			if(checkUp.colorStatus == oppositeTurnColor(cell.colorStatus)){
 				immediateEnemyCount++;
-				alert('checkUp found enemy');
+				//alert('checkUp found enemy');
 				checkUp.toTakeStatus = true;
 				checkEnemies(checkUp, boardLength, goBoard);
 				checkAnyEmptyCounter($scope.globalVar.anyEmptyCounter, $scope.goBoard);
@@ -174,16 +227,16 @@ app.controller('GoBoardController', function($scope){
 			var checkLeft = goBoard[row][col-1];
 			if(checkLeft.colorStatus == 'emptySpot'){
 				immediateEmptyCount++;
-				alert('checkLeft found emptySpot');
+				//alert('checkLeft found emptySpot');
 				checkLeft.checkedStatus = true;
 			}
 			if(checkLeft.colorStatus == cell.colorStatus){
 				immediateFriendlyCount++;
-				alert('checkLeft found friendly');
+				//alert('checkLeft found friendly');
 			}
 			if(checkLeft.colorStatus == oppositeTurnColor(cell.colorStatus)){
 				immediateEnemyCount++;
-				alert('checkLeft found enemy');
+				//alert('checkLeft found enemy');
 				checkLeft.toTakeStatus = true;
 				checkEnemies(checkLeft, boardLength, goBoard);
 				checkAnyEmptyCounter($scope.globalVar.anyEmptyCounter, $scope.goBoard);
@@ -197,16 +250,16 @@ app.controller('GoBoardController', function($scope){
 			var checkRight = goBoard[row][col+1];
 			if(checkRight.colorStatus == 'emptySpot'){
 				immediateEmptyCount++;
-				alert('checkRight found emptySpot');
+				//alert('checkRight found emptySpot');
 				checkRight.checkedStatus = true;
 			}
 			if(checkRight.colorStatus == cell.colorStatus){
 				immediateFriendlyCount++;
-				alert('checkRight found friendly');
+				//alert('checkRight found friendly');
 			}
 			if(checkRight.colorStatus == oppositeTurnColor(cell.colorStatus)){
 				immediateEnemyCount++;
-				alert('checkRight found enemy');
+				//alert('checkRight found enemy');
 				checkRight.toTakeStatus = true;
 				checkEnemies(checkRight, boardLength, goBoard);
 				checkAnyEmptyCounter($scope.globalVar.anyEmptyCounter, $scope.goBoard);
@@ -220,16 +273,16 @@ app.controller('GoBoardController', function($scope){
 			var checkDown = goBoard[row+1][col];
 			if(checkDown.colorStatus == 'emptySpot'){
 				immediateEmptyCount++;
-				alert('checkDown found emptySpot');
+				//alert('checkDown found emptySpot');
 				checkDown.checkedStatus = true;
 			}
 			if(checkDown.colorStatus == cell.colorStatus){
 				immediateFriendlyCount++;
-				alert('checkDown found friendly');
+				//alert('checkDown found friendly');
 			}
 			if(checkDown.colorStatus == oppositeTurnColor(cell.colorStatus)){
 				immediateEnemyCount++;
-				alert('checkDown found enemy');
+				//alert('checkDown found enemy');
 				checkDown.toTakeStatus = true;
 				checkEnemies(checkDown, boardLength, goBoard);
 				checkAnyEmptyCounter($scope.globalVar.anyEmptyCounter, $scope.goBoard);
@@ -239,10 +292,10 @@ app.controller('GoBoardController', function($scope){
 		resetToTakeStatus($scope.goBoard);
 		resetCheckedStatus($scope.goBoard);
 
-		alert('the spots beside this one are: '+'\n'+
-			'empty- '+immediateEmptyCount+'\n'+
-			'friendly- '+immediateFriendlyCount+'\n'+
-			'enemy- '+immediateEnemyCount);
+		//alert('the spots beside this one are: '+'\n'+
+		//	'empty- '+immediateEmptyCount+'\n'+
+		//	'friendly- '+immediateFriendlyCount+'\n'+
+		//	'enemy- '+immediateEnemyCount);
 
 		if(immediateEnemyCount == spotsChecked){
 			$scope.globalVar.enemySurroundFlag = true;
@@ -253,10 +306,10 @@ app.controller('GoBoardController', function($scope){
 		if(immediateEmptyCount > 0){
 			$scope.globalVar.anyEmptyFlag = true;
 		}
-		alert('allEmptyFlag is - ' + $scope.globalVar.allEmptyFlag+'\n'+
-			'anyEmptyFlag is - ' + $scope.globalVar.anyEmptyFlag+'\n'+
-			'enemySurroundFlag is - ' + $scope.globalVar.enemySurroundFlag+'\n'+
-			'tookEnemyFlag is - ' + $scope.globalVar.tookEnemyFlag);
+		//alert('allEmptyFlag is - ' + $scope.globalVar.allEmptyFlag+'\n'+
+		//	'anyEmptyFlag is - ' + $scope.globalVar.anyEmptyFlag+'\n'+
+		//	'enemySurroundFlag is - ' + $scope.globalVar.enemySurroundFlag+'\n'+
+		//	'tookEnemyFlag is - ' + $scope.globalVar.tookEnemyFlag);
 	}
 
 //CHECK FOR ILLEGALS
@@ -272,10 +325,8 @@ app.controller('GoBoardController', function($scope){
 	function checkMultipleIllegal(cell){
 		if($scope.globalVar.anyEmptyFlag == false && $scope.globalVar.tookEnemyFlag == false &&
 			$scope.globalVar.enemySurroundFlag == false){
-			alert('potential illegal move' + '\n' + 'check friendlies for empty spaces'
-				+ '\n' + 'NOT YET IMPLEMENTED');
-			//DO WHAT IT SAYS	check friendly pieces recursively for an empty spot
-			//					if none are found, then it's an illegal move
+			//alert('potential illegal move' + '\n' + 'check friendlies for empty spaces');
+			checkIllegalFriendly(cell, $scope.globalVar.boardLength, $scope.goBoard);
 		}
 	}
 
